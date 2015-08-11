7b2f80b75a50af6ccc008cd72ed7ca986954c5a0
Wladek <wladek@localhost.localdomain>
2015-08-11 13:57:03 -0300

    Refatorando o Código

--- minifit.py
+++ minifit.py
@@ -16,59 +16,19 @@ class Minifit():
             if argv[0].split('.')[-1].lower() == 'js':
                 self.min_js(param)
 
-    def min_css(self, param):
-        nLine = ""
-        copy =[0, True]
-        final = ""
-
+    # open file .CSS or .JS
+    def openFile(self, filename):
         try:
-            # open file .CSS
-            arquivo = open(param)
+            arquivo = open(filename)
+            return(arquivo)
         except Exception:
             print("I did not find the file(s) css / js.\nCheck if the files are in this folder: "+ self.dir)
             exit()
 
-        # read file and remove \n (breaklins)
-        linha = True
-        while linha:
-            linha = arquivo.readline()
-            listaLinha = linha.split('\n')
-            nLine += "".join(listaLinha)
-
-        # remove Coments in CSS and spaces
-        for c in range(1, len(nLine)):
-            if nLine[c - 1] == "/" and nLine[c] == "*":
-                copy[0] += 1
-                copy[1] = False
-
-            if nLine[c - 1] == "*" and nLine[c] == "/":
-                copy[0] -= 1
-                copy[1] = True
-
-            if nLine[c] != ' ' and nLine[c] != '/' and copy == [0, True]:
-                final += nLine[c]
-
-        # I took the file name without the extension and added at the end of the file name ".min.css"
-        name = param.split('.css')[0] + ".min.css"
-        finalFile = open(name, 'w')
-        finalFile.write(final)
-        finalFile.close()
-
-    def min_js(self, param):
-        nLine = ""
-        copy =[0, True]
-        final = ""
-        last = False
-
-        try:
-            # open file .JS
-            arquivo = open(param)
-        except Exception:
-            print("I did not find the file(s) css / js.\nCheck if the files are in this folder: "+ self.dir)
-            exit()
-
-        # read file and remove \n (breaklins) and Coments Line
+    # read file and remove Coments Line
+    def removeLinesComent(self, arquivo):
         lines = True
+        nLine = ""
         while lines:
             lines = arquivo.readline()
             for x in range(0, len(lines)):
@@ -76,29 +36,65 @@ class Minifit():
                     break
                 else:
                     nLine += lines[x]
+        return(nLine)
 
-        # remove Block Coments in JS and spaces
-        for c in range(0, len(nLine)):
-            if nLine[c] == "/" and nLine[c+1] == "*":
+    # remove Block Coments and Line Feed and ' '
+    def removeBlockComent(self, nLine):
+        copy =[0, True]
+        last = False
+        Nremove = False
+        final = ""
+
+        for c in range(0, len(nLine) - 1):
+            if nLine[c] == '/' and nLine[c + 1] == '*':
                 copy[0] += 1
                 copy[1] = False
 
-            if nLine[c - 1] == "*" and nLine[c] == "/":
+            if nLine[c - 1] == '*' and nLine[c] == '/':
                 copy[0] -= 1
                 copy[1] = True
                 last = True
 
-            if nLine[c] != ' ' and copy == [0, True] and nLine[c] != '\t' and nLine[c] != '\n':
+            if nLine[c] == '@':
+                Nremove = True
+
+            if nLine[c] == ' ':
+                if nLine[c-3] == 'v' and nLine[c-2] == 'a' and nLine[c-1] == 'r' or Nremove:
+                    Nremove = False
+                else:
+                    last = True
+
+            if copy == [0, True] and nLine[c] != '\n':
                 if last:
                     last = False
                 else:
                     final += nLine[c]
+        return(final)
 
-        # I took the file name without the extension and added at the end of the file name ".min.js"
-        name = param.split('.js')[0] + ".min.js"
+    # save file name.min.css or name.min.js
+    def saveFileMin(self, nLine, param):
+        if param.split('.')[-1:][0].lower() == 'js':
+            name = '.'.join(param.split('.')[:-1]) + ".min.js"
+        elif param.split('.')[-1:][0].lower() == 'css':
+            name = '.'.join(param.split('.')[:-1]) + ".min.css"
+        
         finalFile = open(name, 'w')
-        finalFile.write(final)
+        finalFile.write(nLine)
         finalFile.close()
+
+    def min_js(self, param):
+        arquivo = self.openFile(param)
+        nLine = self.removeLinesComent(arquivo)
+        nLine = self.removeBlockComent(nLine)
+
+        self.saveFileMin(nLine, param)
+
+    def min_css(self, param):
+        arquivo = self.openFile(param)
+        nLine = arquivo.read()
+        nLine = self.removeBlockComent(nLine)
+
+        self.saveFileMin(nLine, param)
 
 
 if __name__ == "__main__":
